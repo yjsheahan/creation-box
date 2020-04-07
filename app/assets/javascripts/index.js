4d357384079348a54b35197cbf30e8a06d48365d
Yasmin Jones-Sheahan <y.jsheahan@gmail.com>
2020-04-07 16:27:17 +0100

    some lil tweaks for heroku:

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,7 +1,7 @@
 fetch('https://api.mapbox.com/geocoding/v5/mapbox.places/SE1 8NB.json?access_token=pk.eyJ1IjoieWFzbWluanMiLCJhIjoiY2s0MzZhNnduMDM5ZzNucGEzNTJucDkxZyJ9.MvMkfQDy8DyZq5xlPg_f5A')
   .then(function(response) {
     return response.json();
-  });
+  })
   .then(function(data) {
     console.log(data);
   });
